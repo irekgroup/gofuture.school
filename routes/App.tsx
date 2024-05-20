e731d69c63a0e1f185f70165a2ef57a257768d17
Irek Nizamov <70753735+irekgroup@users.noreply.github.com>
2024-05-20 10:39:42 +0300

    Update App.tsx

--- routes/App.tsx
+++ routes/App.tsx
@@ -6,7 +6,7 @@ export default function App({ Component }: PageProps) {
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>GO FUTURE SCHOOL</title>
+        <title>FUTURE SCHOOL</title>
         <link rel="stylesheet" href="/styles.css" />
         <meta
           name="description"
